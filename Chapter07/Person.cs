a9d17e6eeec6c75abffcc7f1a999e515c5abb43f
HARIRAK SRITHAM <harirak@hotmail.co.th>
2020-02-29 21:49:05 +0700

    Chapter07

---
+++ Chapter07/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter07
+{ 
+    
+    class Person
+    {
+        public string name, surname;
+        
+        public int age;
+        public string nationality;
+        public enum GenderP { male=1,female=2};
+
+        public void Walk()
+        {
+
+        }
+
+        public void Run()
+        {
+
+        }
+
+        public void Talk()
+        {
+
+        }
+
+        public void Task()
+        {
+
+        }
+    }
+    
+}
---
+++ Chapter07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person me = new Person();
+            me.name = "HARIRAK";
+            me.surname = "SRITHAM";
+            Person.GenderP a = Person.GenderP.male;
+            Console.WriteLine(a);
+            Console.ReadLine();
+            
+        }
+    }
+}
